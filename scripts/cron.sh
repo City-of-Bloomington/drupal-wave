bc629197947b83611113d228c7283fc97826073a
Cliff Ingham <inghamn@bloomington.in.gov>
2026-04-09 15:07:03 -0400

    Increase WAVE scan rate to every five minutes

---
+++ scripts/cron.sh
@@ -0,0 +1,1 @@
+*/5 * * * * www-data SITE_HOME=/srv/data/webscan cd /srv/sites/webscan/scripts && php wave_scan.php
--- scripts/hourly.sh
+++ scripts/hourly.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 cd /srv/sites/webscan/scripts
-SITE_HOME=/srv/data/webscan php wave_scan.php
 SITE_HOME=/srv/data/webscan php grackle_scan.php
